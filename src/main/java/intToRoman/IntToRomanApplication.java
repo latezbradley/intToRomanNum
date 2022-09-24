ba80720b0265350dc5421d28eac7d643e543caba
Latez Arvel Bradley <bradley.latez@gmail.com>
2022-09-24 13:50:57 -0400

    first commit

---
+++ src/main/java/intToRoman/IntToRomanApplication.java
@@ -0,0 +1,13 @@
+package intToRoman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntToRomanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntToRomanApplication.class, args);
+	}
+
+}
